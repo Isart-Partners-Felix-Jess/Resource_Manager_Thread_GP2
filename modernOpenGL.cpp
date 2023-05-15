e5f5e0e0f9ba723ce15ecab0f8952f0419c9cf94
f.becaud <f.becaud@student.isartdigital.com>
2023-05-15 19:17:18 +0200

    Correct setUp Glad doesn't work yet, assertion neither

--- modernOpenGL.cpp
+++ modernOpenGL.cpp
@@ -1,24 +1,60 @@
 // modernOpenGL.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
-#include <iostream>
-#include <Assertion.hpp>
-#include <GLFW/glfw3.h>
+//#include <iostream>
 #include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <Assertion.hpp>
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	bool test = false;
+	//ASSERT(test, "Test bool is false ! Congrats !");
 
-    bool test = false;
-    ASSERT(test, "Test bool is false ! Congrats !");
-    std::cout << "Hello World!\n";
-    
-    return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//Should go in app class
+	/*FROM HERE*/
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Pipeline", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		processInput(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	/*TO HERE*/
+
+	std::cout << "Program ended!\n";
+
+	return 0;
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
