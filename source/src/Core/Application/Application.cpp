8098c65d346d09bfcc92696bb503ac3ce49af1fd
Felix BECAUD <f.becaud@student.isartdigital.com>
2023-09-13 19:17:15 +0200

    ThReaded ( window name)

--- source/src/Core/Application/Application.cpp
+++ source/src/Core/Application/Application.cpp
@@ -11,7 +11,9 @@ Application::Application(int _width, int _height) : scene(_width, _height)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	window = glfwCreateWindow(_width, _height, "Theaded Resource Manager", NULL, NULL);
+
+											//	 \!/ with an "r" /!\
+	window = glfwCreateWindow(_width, _height, "Threaded Resource Manager", NULL, NULL);
 	if (window == NULL)
 	{
 		glfwTerminate();
